cfa06e94457d1ca0d2dc0c144a2766b313ee6c83
Kim Russel <kimrussel0718@gmail.com>
2026-06-21 01:30:59 +0800

    implemented polymorphism on deposit method to implement interest rewards

--- interest_account_reward.py
+++ interest_account_reward.py
@@ -1,3 +1,7 @@
 from bank_account import BankAccount
 
 class InterestAccountReward(BankAccount):
+    def deposit(self, amount):
+        self._account_balance = self._account_balance + (amount*1.05)
+        print(f"\nDeposit of P{amount:.2f} successful with 5% interest reward.")
+        self.get_balance()
