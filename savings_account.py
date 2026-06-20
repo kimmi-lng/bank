7cd98889f87843dc91251b112b18c0bf507c7344
Kim Russel <kimrussel0718@gmail.com>
2026-06-21 01:54:17 +0800

    added the new method that was inherited, anmed withdraw, in this case we focused on polymorphism of the code, not just to change without reason but to enhance the reusability of the code.

--- savings_account.py
+++ savings_account.py
@@ -4,3 +4,12 @@ class SavingsAccount(InterestAccountReward):
     def __init__(self, account_balance, account_name):
         super().__init__(account_balance, account_name)
         self.fee = 5
+
+    def withdraw(self, amount):
+        deduction = amount + self.fee
+        if self._account_balance >= deduction:
+            self._account_balance = self._account_balance - deduction
+            print(f"\nWithdrawal of P{amount:.2f} successful with P{self.fee:.2f} fee.")
+            self.get_balance()
+        else:
+            print(f"\nInsufficient funds for withdrawal of P{amount:.2f} with P{self.fee:.2f} fee.")
