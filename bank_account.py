6bbe533d50c37014e0abf74a5898d7667a3fe867
Kim Russel <kimrussel0718@gmail.com>
2026-06-21 00:46:17 +0800

    added the get_balance method that siply print the balance of the account

--- bank_account.py
+++ bank_account.py
@@ -4,3 +4,8 @@ class BankAccount(Account):
     def __init__(self, account_balance, account_name):
         self._account_balance = account_balance
         self.account_name = account_name
+
+    def get_balance(self):
+        print(f"\nAccount '{self.account_name}' balance is P{self._account_balance.2f}")
+
+    
