dcd67eaac93a9c1baf87fac2003e70be8bfbe926
Kim Russel <kimrussel0718@gmail.com>
2026-06-21 01:51:26 +0800

    with multi level inheritance, we inherit the interest account reward to new class named savings account.

--- savings_account.py
+++ savings_account.py
@@ -1,1 +1,6 @@
 from interest_account_reward import InterestAccountReward
+
+class SavingsAccount(InterestAccountReward):
+    def __init__(self, account_balance, account_name):
+        super().__init__(account_balance, account_name)
+        self.fee = 5
