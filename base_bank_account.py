0bc0c2ad69616413758f9d2d6220e08eb434ad25
Kim Russel <kimrussel0718@gmail.com>
2026-06-21 00:22:05 +0800

    implemented abstraction, as this forced all account that will be made to have these methods

--- base_bank_account.py
+++ base_bank_account.py
@@ -1,7 +1,11 @@
+from abc import ABC, abstractmethod
+
 class Account():
     
+    @abstractmethod
     def deposit(self, amount):
         pass
 
+    @abstractmethod
     def withdraw(self, amount):
         pass
