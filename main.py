3b5c1ee69ad72cda41101cf0cb71ace85e299e89
Kim Russel <kimrussel0718@gmail.com>
2026-06-21 11:20:00 +0800

    forgot to commit thecreated objects for demonstration and test run

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from bank_account import BankAccount
 from interest_account_reward import InterestAccountReward
 from savings_account import SavingsAccount
+
+kim = BankAccount(6767, "Kim")
+kim.get_balance()
+
+russel = InterestAccountReward(5000, "Russel")
+russel.deposit(1000)
+
+shanny = SavingsAccount(10000, "Shanny")
+shanny.withdraw(2000)
